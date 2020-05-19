8754b3f05f95a8cc71884214755b7aada6661200
athmanemeftahi <38782016+athmanemeftahi@users.noreply.github.com>
2020-05-19 03:15:45 +0200

    Update mauvaises réponses
    - Update

--- app.js
+++ app.js
@@ -7,68 +7,81 @@ var io = require('socket.io').listen(server);
 
 server.listen(8080);
 
-var question = 4;
-var category = 5;
+ // connexion SQL
+ var mysql = require('mysql');
+ var connectionsql = mysql.createConnection(
+   {
+   host     : '109.234.165.84',
+   user     : 'fastfoodproj_quiziby',
+   password : 'mP)=dZ5vm=e1',
+   database: "fastfoodproj_quiziby"
+   }
+ );
+ 
+ connectionsql.connect();
+
+//Initialisation variables
+var question = 1;
+var category = 1;
 var reponses = 4;
+var reponse1 = 0;
+var reponse2 = 0;
+var reponse3 = 0;
+var reponse4 = 0;
+var varsqlbonnereponse = 0;
 
-var mysql = require('mysql');
-var connectionsql = mysql.createConnection({
-host     : '109.234.165.84',
-user     : 'fastfoodproj_quiziby',
-password : 'mP)=dZ5vm=e1',
-database: "fastfoodproj_quiziby"
-});
-
+//Port socket OK
 io.sockets.on('connection', function (socket) {
     console.log('client connecté');
 });
 
+//Randomisation variables selon constantes
  var questionchoose = Math.floor(Math.random() * question) + 1;
+ console.log("ID question: "+questionchoose);
 
  var categorychoose = Math.floor(Math.random() * category) + 1;
+ console.log("ID catégorie: "+categorychoose);
 
  var reponsechoose = Math.floor(Math.random() * reponses) + 1;
+ console.log("Emplacement de la réponse: "+reponsechoose);
 
-var reponse1 = null;
-var reponse2 = null;
-var reponse3 = null;
-var reponse4= null;
-var sqlreponse1=null;
-var sqlreponse2=null;
+//Chercher la bonne réponse de la question
+var queryString = "SELECT id_answer FROM questions WHERE id_question="+questionchoose;
 
-connectionsql.connect(function(err) {
+connectionsql.query(queryString, function(err, rows, fields) {
   if (err) throw err;
-  connectionsql.query("SELECT id_answer FROM questions WHERE id_question="+questionchoose, function (err, sqlreponse1, fields) {
-    if (err) throw err;
-    console.log(sqlreponse1);
-  });
+  varsqlbonnereponse = rows[0].id_answer;
+  console.log("ID Bonne réponse: "+varsqlbonnereponse);
 });
 
-// INSERT INTO `questions` (`id_question`, `id_answer`, `id_category`) VALUES ('1', '4', '1');
-
-// bonne réponse dans la bonne variable
+// Bonne réponse dans choix du joueur
 if (reponsechoose === 1) {
-  var reponse1 = varsqlreponse1;
+  var reponse1 = varsqlbonnereponse;
 } else if (reponsechoose === 2) {
-  var reponse2 = varsqlreponse1;
+  var reponse2 = varsqlbonnereponse;
 } else if (reponsechoose === 3) {
-  var reponse3 = varsqlreponse1;
+  var reponse3 = varsqlbonnereponse;
 } else if (reponsechoose === 4) {
-  var reponse4 = varsqlreponse1;
+  var reponse4 = varsqlbonnereponse;
 }
 
-// Mauvaises réponses
-if (reponse1 == null) {
-  var varok = 0;
-  while(varok==0)
+for (varbreak = 0; varbreak < 1; varbreak++) {
+  var queryString = "SELECT id_answer FROM answers WHERE id_category="+categorychoose+" LIMIT 1";
+  connectionsql.query(queryString, function(err, rows, fields)
   {
-    connectionsql.connect(function(err) {
-      if (err) throw err;
-      connectionsql.query("SELECT id_answer FROM questions WHERE id_question="+questionchoose, function (err, sqlreponse2, fields) {
-        if (varsqlreponse1 != varsqlreponse2) varok = 1;
-        if (err) throw err;
-        console.log(sqlreponse2);
-      });
-    }); 
+    if (err) throw err;
+    if (varsqlbonnereponse != rows[0].id_answer){
+      console.log('varbreak: ', varbreak);
+      reponse1 = rows[0].id_answer;
+      console.log('ID bonne réponse 1: ', reponse1);
+    }
+    else
+    {
+      varbreak=varbreak-1;
+    }
   }
+  );
 }
+
+//Fin du code
+console.log("--- Fin du code ---");
